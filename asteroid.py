4c1da5bfe365635672f18205f82ecd9d2f87f2b1
FatherOfSnails <fatherofsnails@gmail.com>
2025-07-22 02:51:01 -0400

    G: Asteroid setup part 2

--- asteroid.py
+++ asteroid.py
@@ -1,19 +1,19 @@
-import circleshape, pygame
+import pygame
+from circleshape import CircleShape
+from constants import *
 
-class Asteroid(circleshape.Circleshape):
+class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
+        super().__init__(x, y, radius)
     
     def draw(self, screen):
         pygame.draw.circle(
             screen,
-            (1,1,1), 
-            (self.x, self.y), 
+            (255,255,255),
+            self.position,
             self.radius,
             2
         )
     
     def update(self, dt):
-        pass
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
+
     Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (1,1,1))
         updatable.update(dt)
+
+        screen.fill((1,1,1))
+
         for drawables in drawable:
             drawables.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
