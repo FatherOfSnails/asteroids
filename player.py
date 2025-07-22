4df8c81e442f7a644c57c4f7aab2ce82a70eb3f0
FatherOfSnails <fatherofsnails@gmail.com>
2025-07-22 05:30:36 -0400

    I: Gave the player the ability to shoot

--- player.py
+++ player.py
@@ -1,8 +1,10 @@
-import circleshape, constants, pygame
+import circleshape, pygame
+from shot import Shot
+from constants import *
 
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
-        super().__init__(x, y, constants.PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
     def triangle(self):
@@ -17,11 +19,15 @@ class Player(circleshape.CircleShape):
         pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
     
     def rotate(self, dt):
-        self.rotation += constants.PLAYER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
     
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * constants.PLAYER_SPEED * dt
+        self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -34,3 +40,6 @@ class Player(circleshape.CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
